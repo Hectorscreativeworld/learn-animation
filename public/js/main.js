ad558fd0d8a152bfd226341a46bd63cd25815c7c
hectorscreativeworld <hectorscreativeworld@gmail.com>
2019-08-20 15:51:00 -0400

    floor leaves and tree

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,88 @@
 /*  Autumn Greeting Card -- js */
 
-(function($){
-	'use strict';
+;(function($) {
+  'use strict'
 
-	// declare actors here
+  // declare actors here//
+  var $backFallingLeaves = $('#brownLeaf, #orangeLeaf, #redLeaf'),
+    $textLine1 = $('.text-line-1'),
+    $textLine2 = $('.text-line-2'),
+    $textGreeting = $('.text-greeting'),
+    $treeLeaves = $('[id^=treeleaf]'),
+    $floorLeaves = $('[id^=floorleaf]'),
+    $bird = $('#Bird'),
+    $birdHat = $bird.find('#BirdHat'),
+    $birdEyes = $bird.find('#leftEye, #rightEye'),
+    $nest = $('#NestAndLeaves'),
+    $tree = $('#tree_trunk'),
+    $cardContainer = $('.card.container')
 
-	// clear stage 
+  // clear stage //
+  function clearStage() {
+    var clearTl = new TimelineMax()
 
-	// enter floor vegetation
+    clearTl
+      .set($backFallingLeaves, { autoAlpha: 0 })
+      .set($textLine1, { autoAlpha: 0 })
+      .set($textLine2, { autoAlpha: 0 })
+      .set($textGreeting, { autoAlpha: 0 })
+      .set($treeLeaves, { autoAlpha: 0 })
+      .set($bird, { y: '+=65', autoAlpha: 0 })
+      .set($nest, { autoAlpha: 0 })
+      .set($tree, { autoAlpha: 0 })
+      .set($floorLeaves, { y: '+=275', onComplete: showContainer })
 
-	// enter tree
+    function showContainer() {
+      $cardContainer.css('display', 'block')
+    }
 
-	// enter the greeting text
-	
-	// the GO function ...to kick things all off
-	
+    return clearTl
+  }
 
-})(jQuery);
+  // enter floor vegetation//
+  function enterFloorVegetation() {
+    var fleavesTl = new TimelineMax()
 
+    fleavesTl
+      .staggerTo($floorLeaves, 1, { y: 0, ease: Back.easeInOut }, 0.01)
+      .fromTo(
+        $tree,
+        1.1,
+        { scaleY: 0.2, autoAlpha: 0, transformOrigin: 'center bottom' },
+        {
+          scaleY: 1,
+          autoAlpha: 1,
+          transformOrigin: 'center bottom',
+          ease: Back.easeInOut
+        }
+      )
+      .fromTo(
+        $tree,
+        0.9,
+        { scaleX: 0.2, autoAlpha: 0, transformOrigin: 'center bottom' },
+        {
+          scaleX: 1,
+          autoAlpha: 1,
+          transformOrigin: 'center bottom',
+          ease: Back.easeInOut
+        },
+        '-=0.9'
+      )
+    return fleavesTl
+  }
 
+  // enter tree//
+
+  // enter the greeting text//
+
+  // the GO function ...to kick things all off//
+  function go() {
+    console.log('go....')
+    var masterTl = new TimelineMax()
+
+    masterTl
+      .add(clearStage(), 'scene-clear-stage')
+      .add(enterFloorVegetation(), 'scene-floor-vegetation')
+  }
+  go()
+})(jQuery)
